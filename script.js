196e992101e2eaf86e89c81662b88e1788ed8b9c
TyHil <gotyhill@gmail.com>
2023-08-09 00:25:45 +0000

    Use global CSS and JS

--- script.js
+++ script.js
@@ -594,12 +594,3 @@ function generate() {
 }
 
 generate();
-
-
-
-/* Google Analytics */
-
-window.dataLayer = window.dataLayer || [];
-function gtag() { window.dataLayer.push(arguments); }
-gtag("js", new Date());
-gtag("config", "G-BVTJ5JS3H2");
